571cdaa6008dcdd70c6bdf83ea31dd5b1895c52b
faqihrofiqi <21sa1076@mhs.amikompurwokerto.ac.id>
2024-11-26 23:58:18 +0700

    feat(seed): add dummy users

---
+++ database/seeders/20241126164358-seed-assignmentsubmissions.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('assignmentsubmissions', [
+      {
+        assignment_id: 1,
+        student_id: 101,
+        submission_content: 'JavaScript exercise solutions.',
+        status: 'graded',
+        grade: 85.5,
+        feedback: 'Good job! Improve your variable naming.',
+        submitted_at: new Date('2024-12-01T10:00:00'),
+        graded_at: new Date('2024-12-02T15:30:00'),
+      },
+      {
+        assignment_id: 2,
+        student_id: 102,
+        submission_content: 'SQL join examples.',
+        status: 'pending',
+        submitted_at: new Date('2024-12-03T14:00:00'),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('assignmentsubmissions', null, {});
+  },
+};
