e25cf597bdbc35f8dd6f9eb580cf9995c112691b
faqihrofiqi <21sa1076@mhs.amikompurwokerto.ac.id>
2024-11-27 00:05:35 +0700

    masuk api, app

--- api.js
+++ api.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-<<<<<<< HEAD
-const authRoutes = require('./routes/route'); // Correct import path for routes
-=======
 const authRoutes = require('./routes/routes'); // Import routes
->>>>>>> 28ba45baf9fb0790aa8894fb2e0122cc4ebac2f0
 const db = require('./database/models'); // Import database models
 
 const app = express();
